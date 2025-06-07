2163afe62ae010868f34fff3a8aacda7180c5715
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-06-07 21:59:52 +0800

    添加例子。

--- CSharpFunctionalProgrammingSamples/EntryPoint.cs
+++ CSharpFunctionalProgrammingSamples/EntryPoint.cs
@@ -1,7 +1,5 @@
 ﻿// 这里替换为目标的那个案例的包裹类型。
-var sample = new Lesson12_AnonymousFunctionCaptureSample();
+var sample = new Lesson13_DelegateVarianceSample();
 
 // 调用。
 sample.RunSample();
-
-Console.ReadKey();
