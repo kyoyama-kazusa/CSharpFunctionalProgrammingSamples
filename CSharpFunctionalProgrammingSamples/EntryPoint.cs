adb9c1359905f6ed14a92499b544842b97d073f2
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-07-12 23:29:08 +0800

    添加例子。

--- CSharpFunctionalProgrammingSamples/EntryPoint.cs
+++ CSharpFunctionalProgrammingSamples/EntryPoint.cs
@@ -1,5 +1,5 @@
 ﻿// 这里替换为目标的那个案例的包裹类型。
-var sample = new Lesson21_GenericTypeParameterVarianceSample();
+var sample = new Lesson22_AsyncLambdaExpressionSample();
 
 // 调用。
 sample.RunSample();
---
+++ CSharpFunctionalProgrammingSamples/Lesson22_AsyncLambdaExpressionSample.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable
+
+using System.Net.Http;
+
+namespace CSharpFunctionalProgrammingSamples;
+
+/// <summary>
+/// 异步 lambda 表达式。
+/// </summary>
+internal sealed class Lesson22_AsyncLambdaExpressionSample : Sample
+{
+	/// <inheritdoc/>
+	public override async void RunSample()
+	{
+		// 异步 lambda 表达式。
+		Func<Task> fetcher = async () =>
+		{
+			using var httpClient = new HttpClient();
+			try
+			{
+				var html = await httpClient.GetStringAsync("https://example.com/");
+				Console.WriteLine(html);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"错误信息：{ex.Message}");
+			}
+		};
+
+		// 执行异步 lambda 表达式 - 这个时候才会执行异步 lambda 表达式。
+		await fetcher();
+	}
+}
