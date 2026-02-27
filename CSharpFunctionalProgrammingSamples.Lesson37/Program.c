b503c66ede8fb8489f067ffe3635bacbaf3c0af9
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-02-27 20:35:38 +0800

    添加内容。

---
+++ CSharpFunctionalProgrammingSamples.Lesson37/Program.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	const char* const name = "Kazusa";
+	int age = 15;
+	printf("Hello everyone! My name is %s, and I'm %d years old!\n", name, age);
+	return 0;
+}
